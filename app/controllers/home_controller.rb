ffacce118cffd918795028788e6ec8e1072a6f5b
Bennis Yiu <bennisyiu@gmail.com>
2021-06-01 19:20:53 +0800

    added prices to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,24 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
+    # Get price data
+    @price_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD'
+    @price_uri = URI(@price_url)
+    @price_response = Net::HTTP.get(@price_uri)
+    @prices = JSON.parse(@price_response)
+
+    # Get news data
+
     @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @news = JSON.parse(@response)
+
   end
 
   def prices
+
+
+
   end
 end
